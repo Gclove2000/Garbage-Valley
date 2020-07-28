161e2aa29e1ced6d38dce0aa2e533e292033d004
YMYGABE <51685770+YMYGABE@users.noreply.github.com>
2020-07-28 15:14:04 +0800

    增加背包，还剩下点明天搞
    背包打开的功能，可以增加预制体到背包上

---
+++ Garbage Valley/Assets/Script/Bag/Bag_Item/itemOnWorld.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class itemOnWorld : MonoBehaviour
+{
+    public Inventory ItemOfBag;
+    public Item thisItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ADD();
+            Destroy(gameObject);
+        }
+    }
+    public void ADD()
+    {
+        if (!ItemOfBag.MyBag.Contains(thisItem))
+        {
+            for (int i = 0; i < ItemOfBag.MyBag.Count; i++)
+            {
+                if(ItemOfBag.MyBag[i] == null)
+                {
+                    ItemOfBag.MyBag[i] = thisItem;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            thisItem.item_num++;
+        }
+        InventoryManager.Refreash();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Garbage Valley/Assets/Script/Bag/Inventory/InventoryManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+    [Header("所调用的背包")]
+    public Inventory ThisBag;
+
+
+    [Header("slot相关设置")]
+    public GameObject slotGrid;
+    public GameObject emptySlot;
+
+    [Header("放置Slot的列表")]
+    public List<GameObject> Items = new List<GameObject>();
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this);
+        }
+        instance = this;
+    }
+    private void OnEnable()
+    {
+        Refreash();
+    }
+    public static void Refreash()
+    {
+        for (int i = 0; i < instance.slotGrid.transform.childCount; i++)
+        {
+            if(instance.slotGrid.transform.childCount == 0)
+            {
+                break;
+            }
+            else{
+                Destroy(instance.transform.GetChild(i).gameObject);
+                instance.Items.Clear();
+            }
+        }
+
+        for (int i = 0; i < instance.ThisBag.MyBag.Count; i++)
+        {
+            instance.Items.Add(Instantiate(instance.emptySlot));
+            instance.Items[i].transform.SetParent(instance.slotGrid.transform);
+        }
+    }
+}
