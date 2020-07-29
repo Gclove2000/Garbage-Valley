b5535596a53646587adb2abdea7ea956b761996d
Gclove2000 <1123448674@qq.com>
2020-07-29 18:22:14 +0800

    1

--- Garbage Valley/Assets/Script/NPC/DialogSystem_1.cs
+++ Garbage Valley/Assets/Script/NPC/DialogSystem_1.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Dai : MonoBehaviour
+public class DialogSystem_1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("UI组件")]
+    public Text textLabel;
+    public Image face;
+
+    [Header("文本文件")]
+    public TextAsset textFile;
     void Start()
     {
         
