2e03e845695a11b737f5fa2aa8c23694824eae82
Gclove2000 <1123448674@qq.com>
2020-07-31 14:41:58 +0800

    1

--- Garbage Valley/Assets/Script/Player/CameraFellow.cs
+++ Garbage Valley/Assets/Script/Player/CameraFellow.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class CameraFellow : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform target;//相机位置
+    public Transform target;//人物位置
+    [Header("确定相机的范围")]
+    public Vector2 leftDown;
+    public Vector2 rightUp;
     public float smooting;
+    public Vector2 test;
     void Start()
     {
         
@@ -18,6 +22,8 @@ public class CameraFellow : MonoBehaviour
             if(transform.position != target.position)
             {
                 Vector3 targetPos = target.position;
+                targetPos.x = Mathf.Clamp(targetPos.x, leftDown.x, rightUp.x);
+                targetPos.y = Mathf.Clamp(targetPos.y, leftDown.y, rightUp.y);
                 transform.position = Vector3.Lerp(transform.position, targetPos, smooting);
 
             }
--- Garbage Valley/Assets/Script/Player/PlayerController.cs
+++ Garbage Valley/Assets/Script/Player/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
 
     private BoxCollider2D myBody;
     private bool isNPC;//判断pnc
+
+    public Transform rightUp;
+    public Transform leftDown;
     void Start()
     {
         myBody = GetComponent<BoxCollider2D>();
@@ -23,14 +26,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Filp();
+       
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
         //读取x,y和magnitude参数，实现动画的控制
         animatior.SetFloat("Horizontal", movement.x);
         animatior.SetFloat("Vertical", movement.y);
         animatior.SetFloat("Magnitude", movement.magnitude);
         transform.position = transform.position + speed*movement * Time.deltaTime;
+        moveLimit();
     }
-
+    void moveLimit()
+    {
+        Vector3 targetPos = transform.position;
+        targetPos.x = Mathf.Clamp(targetPos.x, leftDown.position.x, rightUp.position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, leftDown.position.y, rightUp.position.y);
+        transform.position = Vector3.Lerp(transform.position, targetPos, 0.1f);
+    }
     void Filp()//水平翻转
     {
         if (animatior.GetFloat("Horizontal") < -0.1f)
